c55f620fcc87d1a7bb8d490dd220087ed2fde671
AnthonyPCPortable <anth06ny@gmail.com>
2026-07-10 15:10:56 +0200

    Fin TP Donnée

--- app/src/main/java/com/amonteiro/a06_ipi_lyon_android/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/amonteiro/a06_ipi_lyon_android/presentation/viewmodel/MainViewModel.kt
@@ -22,6 +22,7 @@ suspend fun main() {
 
     //Affichage de la liste (qui doit être remplie) contenue dans la donnée observable
     println("List : ${viewModel.dataList.value}")
+    println("ErrorMessage : ${viewModel.errorMessage.value}")
 
     //Pour que le programme s'arrête, inutile sur Android
     WeatherApiDataSource.close()
@@ -31,14 +32,24 @@ class MainViewModel : ViewModel() {
     //MutableStateFlow est une donnée observable
     val dataList = MutableStateFlow(emptyList<Weather>())
     val runInProgress = MutableStateFlow(false)
+    val errorMessage = MutableStateFlow("")
 
-    suspend fun loadWeathers(cityName: String) {
+    fun loadWeathers(cityName: String) {
 
         runInProgress.value = true
 
         viewModelScope.launch(Dispatchers.IO) {
-            dataList.value = WeatherApiDataSource.loadWeathers(cityName)
-            runInProgress.value = false
+            try {
+                dataList.value = WeatherApiDataSource.loadWeathers(cityName)
+            } catch (e: Exception) {
+                //Affiche la stacktrace
+                e.printStackTrace()
+                errorMessage.value = e.message ?: "Une erreur est survenue"
+            }
+            finally {
+                runInProgress.value = false
+            }
         }
+
     }
 }
